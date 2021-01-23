5563f956340a3e972c9fd048544f1e6459d5d15c
Jimeux <jimeux@gmail.com>
2021-01-23 18:36:56 +0900

    Update text

--- user-service/main.go
+++ user-service/main.go
@@ -59,7 +59,7 @@ func main() {
 func Test(w http.ResponseWriter, r *http.Request) {
 	res := struct {
 		Msg string `json:"msg"`
-	}{Msg: fmt.Sprintf("Welcome to the user-service 💀️🙋🏻💀️ (%s)", r.RequestURI)}
+	}{Msg: fmt.Sprintf("Welcome to the Built Minimally™ user-service 💀️🙋🏻💀️ (%s)", r.RequestURI)}
 
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(res); err != nil {
